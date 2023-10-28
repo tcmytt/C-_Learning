c65366c3a7eadac26c2d3cda706b610c141afc1e
NguyenHiep <tcmytt@gmail.com>
2023-10-28 20:58:35 +0700

    28 / 10

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cau_Truc_Lenh_Co_Ban
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
